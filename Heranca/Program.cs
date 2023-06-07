fbbda2542ada9b9f054067226cee845ca84a47c0
jonat <jonat@DESKTOP-660J8J1>
2023-06-07 14:08:02 -0300

    Adicionar arquivos de projeto.

---
+++ Heranca/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Heranca.Entities;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount businessAccount = new BusinessAccount(8010,"Bob Brown",100.0,500.0);
+            Console.WriteLine(businessAccount.Balance);
+
+        }
+    }
+}
